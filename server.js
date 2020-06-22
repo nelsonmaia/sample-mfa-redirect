d0a31e6fe5aa16d4f82539f29511f004f9116466
nellson.maia@gmail.com <nellson.maia@gmail.com>
2020-06-22 16:52:54 +0100

    make it better

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const { join } = require("path");
 const morgan = require("morgan");
 const app = express();
+const path = require('path')
 
 const port = process.env.PORT || 3000;
 
